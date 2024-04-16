0c34fd2c50ea0fbca74d8f41a29c0dce0d698450
Rayovaac <160878772+Rayovaac@users.noreply.github.com>
2024-04-16 12:17:57 -0300

    Update Ex01_calculadora_IMC.py

--- trabalhos/Ex01_calculadora_IMC.py
+++ trabalhos/Ex01_calculadora_IMC.py
@@ -5,23 +5,21 @@ imc = (peso/altura**2)
 print(imc)
 
 if peso<=0 and altura<=0:
-    print("O numero e negativo Por favor Digite Algo valido Arrombado")
+    print("Digite algo valido")
 
 if imc<=18.5:
-    print("Vai se alimentar Tu Vai morrer")
+    print("Abaixo do peso")
 
 elif imc>=18.5 and imc<=24.9:
-    print("Boa Ta Na media")
+    print("Peso normal")
 elif imc>=25 and imc<=29.9:
-    print("Ta quase thais Carla")
+    print("Sobrepeso")
 elif imc>=30 and imc<=34.9: 
     peso_perdido=peso*0.2
-    print("Se voce Não fazer seu Cardio Vai virar a Thais Carla",peso_perdido)
-elif imc>=35 and imc<=39.9:
+    print("Obesidade grau I",peso_perdido)
+elif imc>=35 and imc<=39.9: 
     peso_perdido=peso*0.3
-    print("Ta se Transformando nela, vo te come vo te come",peso_perdido)
+    print("Obesidade grau II",peso_perdido)
 else:
     peso_perdido=peso*0.4
-    print("Viro a Thais Carla Pique Majin Boo",peso_perdido)
-
-    
+    print("Obesidade grau III",peso_perdido)
